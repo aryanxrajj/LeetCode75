b8e2b74c2b2a9452a8ce6891f95796e4cbd0211d
Aryan Raj <aryanxrajj@gmail.com>
2025-03-10 13:53:37 +0530

    LeetCode

--- src/TwoPointers/MoveZeros.java
+++ src/TwoPointers/MoveZeros.java
@@ -1,4 +1,19 @@
 package TwoPointers;
 
 public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        if(nums.length == 1){
+            return;
+        }
+
+        int left = 0;
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i]!= 0){
+                int temp = nums[left];
+                nums[left] = nums[i];
+                nums[i] = temp;
+                left++;
+            }
+        }
+    }
 }
