cc8dad9c3891483e3047d35a61f1df9299f1fe9e
Aryan Raj <aryanxrajj@gmail.com>
2025-04-09 21:45:34 +0530

    LeetCode

---
+++ src/LinkedList/OddEvenLinkedList.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+public class OddEvenLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(2);
+        head.next.next.next.next.next.next = new ListNode(6);
+
+        ListNode result = oddEvenList(head);
+        while(result!=null){
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+
+    public static ListNode oddEvenList(ListNode head) {
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return head;
+    }
+}
